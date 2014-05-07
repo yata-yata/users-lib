0eb675ecbc36b61931f0bf6ba62f2bc57f57815f
Mike Davis <mike@michaeljacobdavis.com>
2014-05-06 23:04:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# user-lib
+
+Users Persistence Library
---
+++ test/index.js
@@ -0,0 +1,19 @@
+// Load modules
+
+var Lab = require('lab'),
+
+  // Declare internals
+  internals = {},
+
+  // Test aliases
+  expect = Lab.expect,
+  before = Lab.before,
+  beforeEach = Lab.beforeEach,
+  after = Lab.after,
+  afterEach = Lab.afterEach,
+  describe = Lab.experiment,
+  it = Lab.test,
+  assert = Lab.assert;
+
+describe('users-lib', function () {
+});
